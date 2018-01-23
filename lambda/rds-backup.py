d9b67b7604ac784a1d40708234a883630d1c5bf6
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2018-01-22 20:59:43 -0400

    ebs backup is ready to work

--- lambda/rds-backup.py
+++ lambda/rds-backup.py
@@ -1,21 +1,28 @@
 import boto3
 import datetime
 
-# https://gist.github.com/mzupan/41d01bfb3b4c292fdac0
 
 def lambda_handler(event, context):
+
     print("Connecting to RDS")
     client = boto3.client('rds')
-    
-    print("RDS snapshot backups stated at %s...\n" % datetime.datetime.now())
-    client.create_db_snapshot(
-        DBInstanceIdentifier='web-platform-slave', 
-        DBSnapshotIdentifier='web-platform-%s' % datetime.datetime.now().strftime("%y-%m-%d-%H"),
-        Tags=[
-            {
-                'Key': 'backupRetention',
-                'Value': '1825'
-            },
-        ]
-    )
-    
+
+    for rds in client.describe_db_instances()['DBInstances']:
+        dbinstancearn = rds['DBInstanceArn']
+        dbinstanceid = rds['DBInstanceIdentifier']
+        tagsdic = {}
+
+        for tags in client.list_tags_for_resource(ResourceName=dbinstancearn)['TagList']:
+            tagsdic[tags['Key']] = tags['Value']
+
+        if ('backup' in tagsdic and tagsdic['backup'] == 'si'):
+            starttime = datetime.datetime.now().strftime("%y-%m-%d-%H:%M")
+            print("RDS snapshot backups to: {0:s} started at: {1:s}".format(dbinstanceid, starttime))
+            today = datetime.datetime.now().strftime("%Y%m%d%H%M")
+            snapname = "{0:s}-{1:s}".format(dbinstanceid, today)
+            client.create_db_snapshot(
+                DBInstanceIdentifier=dbinstanceid,
+                DBSnapshotIdentifier=snapname
+            )
+        else:
+            print("No RDS snapshot to: {0:s}".format(dbinstanceid))
