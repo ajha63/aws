70e01ba34447e9b5531d4241c2bf6e76ee059121
Alvaro Hernandez <hernandez.alvaro@gmail.com>
2018-01-23 11:45:05 -0400

    Improve tags use in rds_backup.py

--- lambda/rds-backup.py
+++ lambda/rds-backup.py
@@ -1,7 +1,6 @@
 import boto3
 import datetime
 
-
 def lambda_handler(event, context):
 
     print("Connecting to RDS")
@@ -15,14 +14,38 @@ def lambda_handler(event, context):
         for tags in client.list_tags_for_resource(ResourceName=dbinstancearn)['TagList']:
             tagsdic[tags['Key']] = tags['Value']
 
-        if ('backup' in tagsdic and tagsdic['backup'] == 'si'):
-            starttime = datetime.datetime.now().strftime("%y-%m-%d-%H:%M")
-            print("RDS snapshot backups to: {0:s} started at: {1:s}".format(dbinstanceid, starttime))
-            today = datetime.datetime.now().strftime("%Y%m%d%H%M")
-            snapname = "{0:s}-{1:s}".format(dbinstanceid, today)
+        if (('backup' in tagsdic) and (tagsdic['backup'] == 'True')):
+            # Get retention day tag
+            if 'retentiondays' in tagsdic:
+                retentionDays = tagsdic['retentiondays']
+            else: 
+                retentionDays = 365
+            # Get deletesnap tag
+            if 'deletesnapshot' in tagsdic:
+                if tagsdic['deletesnapshot']:
+                    deleteSnap = 'True'
+                else:
+                    deleteSnap = 'False'
+            else:
+                deleteSnap = 'False'
+
+            rightnow = datetime.datetime.now().strftime("%Y-%m%d%H%M")
+            snapname = "{0:s}-{1:s}".format(dbinstanceid, rightnow)
+            
+            print("RDS snapshot backups to: {0:s} started at: {1:s}".format(dbinstanceid, rightnow))
             client.create_db_snapshot(
                 DBInstanceIdentifier=dbinstanceid,
-                DBSnapshotIdentifier=snapname
+                DBSnapshotIdentifier=snapname,
+                Tags = [
+                    {
+                        'Key': 'retentiondays',
+                        'Value': retentionDays
+                    },
+                    {
+                        'Key': 'deletesnapshot',
+                        'Value': str(deleteSnap)
+                    },
+                ]
             )
         else:
             print("No RDS snapshot to: {0:s}".format(dbinstanceid))
